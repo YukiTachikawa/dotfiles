575f0895556d8bc0012eccab361e4eac2d2a64f3
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2024-04-08 17:39:31 +0900

    fix init.sh

--- init.sh
+++ init.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-EXCLUDE_DIRS=(".git" ".config" ".github")
 
-for dotfile in "${SCRIPT_DIR}"/.?*; do
-    base_dotfile=$(basename "$dotfile")
-    [[ " ${EXCLUDE_DIRS[@]} " =~ " ${base_dotfile} " ]] && continue
-    ln -snfv "$dotfile" "$HOME/$base_dotfile" 
+for f in .??*; do
+    [[ "$f" == ".git" ]] && continue
+    [[ "$f" == ".github" ]] && continue
+    [[ "$f" == ".config" ]] && continue
+    ln -snfv "$f" "$HOME/$f" 
 done
  
 function link_directory {
     local source_dir="$1"
-    local dest_dir="$HOME/${source_dir#$SCRIPT_DIR/}" 
+    local dest_dir="$HOME/${source_dir#$SCRIPT_DIR/}"
     find "$source_dir" -type f ! -path '*.git*' | while IFS= read -r file; do 
         local dest_path="${dest_dir}/${file#$SCRIPT_DIR/}"
         mkdir -p "$(dirname "$dest_path")"
