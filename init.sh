805e07fb0373621a0d433b1e4c4881dc08bf3b41
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2024-04-08 16:56:58 +0900

    add .config

--- init.sh
+++ init.sh
@@ -6,3 +6,17 @@ for dotfile in .??*; do
     ln -snfv  "$SCRIPT_DIR"/"$dotfile" ~/"$dotfile"
 done
  
+
+DOT_DIRECTORY="${PWD}"
+declare -a DIRECTORIES=(".config")
+for child_directory in $DIRECTORIES; do
+  cd "${DOT_DIRECTORY}/${child_directory}"
+
+  for directory in $(find . -type d); do
+    mkdir -p "${HOME}/${child_directory}/${directory}"
+  done
+
+  for file in $(find . -type f | grep -v .git); do
+      ln -snfv "${DOT_DIRECTORY}/${child_directory}/${file:2}" "${HOME}/${child_directory}/${file:2}"
+  done
+done
