c294740b609cfd6eda7c5f9c348a47e460e26918
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2024-04-13 20:05:49 +0900

    feat(nvim): ctrl+/ to toggle comments

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -246,7 +246,15 @@ require('lazy').setup({
   --    require('Comment').setup({})
 
   -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim', opts = {} },
+  { 'numToStr/Comment.nvim', 
+    opts = {
+      toggler = {
+        line = '<C-_>',
+      },
+      opleader = {
+        line = '<C-_>',
+      },
+  } },
 
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
